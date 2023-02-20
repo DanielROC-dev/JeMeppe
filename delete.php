37d805c4beface72594dbf70d10ea91d5505c856
daniel <danieluitalmere@gmail.com>
2023-02-20 16:17:09 +0100

    file transfer

--- delete.php
+++ delete.php
@@ -1,15 +1,47 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "mydb";
+if ($_POST['action'] === 'delete') {
+  // Connect to the database
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "mydb";
 
-    $conn = mysqli_connect("$servername", "$username", "$password", "$dbname");
-    $id = $_GET['id'];
-    // Delete the row with the specified id from the "kamer" table
-    $sql = ;
-    mysqli_query($conn, "DELETE FROM kamer WHERE id=$id");
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Redirect to the index page
-    header("Location:index2.php");
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  // Delete data from the link table
+  $linkTable = "kamervoorz";
+  $deleteLinkTable = "DELETE FROM " . $linkTable;
+  if ($conn->query($deleteLinkTable) === TRUE) {
+    echo "Data deleted from the link table successfully";
+  } else {
+    echo "Error deleting data from the link table: " . $conn->error;
+  }
+
+  // Delete data from the first table
+  $firstTable = "kamer";
+  $deleteFirstTable = "DELETE FROM " + $firstTable;
+  if ($conn->query($deleteFirstTable) === TRUE) {
+    echo "Data deleted from the first table successfully";
+  } else {
+    echo "Error deleting data from the first table: " . $conn->error;
+  }
+
+  // Delete data from the second table
+  $secondTable = "second_table_name";
+  $deleteSecondTable = "DELETE FROM " + $secondTable;
+  if ($conn->query($deleteSecondTable) === TRUE) {
+    echo "Data deleted from the second table successfully";
+  } else {
+    echo "Error deleting data from the second table: " . $conn->error;
+  }
+
+  $conn->close();
+}
+?>
 ?>
